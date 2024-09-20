77daf933bd4e81e2c10eefb32a1f20dc6a5546ed
jafish <josh@joshfishburn.com>
2024-09-20 12:50:33 -0400

    Challenge Step 2 (Crouch) Completed

--- 2024-09-20-Challenge-1-1-Solution/Assets/FPSInput.cs
+++ 2024-09-20-Challenge-1-1-Solution/Assets/FPSInput.cs
@@ -1,29 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 // basic WASD-style movement control
 // commented out line demonstrates that transform.Translate instead of charController.Move doesn't have collision detection
 
+
 [RequireComponent(typeof(CharacterController))]
 [AddComponentMenu("Control Script/FPS Input")]
-public class FPSInput : MonoBehaviour {
+public class FPSInput : MonoBehaviour
+{
 	public float gravity = -9.8f;
 	public float walkSpeed = 6.0f;
 	public float sprintSpeed = 12f;
-	private float speed;
+	public float crouchSpeed = 3f;
+	public float crouchHeight = 1f;
+	public KeyCode crouchKey = KeyCode.C;
 
+	// "private" access-control keyword used because these variables are managed internally to the script
+	// i.e. they don't need to be accessed by other scripts or edited in the inspector (and actually it's very
+	// important that they not be!)
+	private float speed;
+	private bool crouching;
+	private float originalHeight;
+	private Vector3 originalCenter;
 	private CharacterController charController;
-	
-	void Start() {
+
+	void Start()
+	{
 		charController = GetComponent<CharacterController>();
+		originalHeight = charController.height;
+		originalCenter = charController.center;
 	}
-	
-	void Update() {
-		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+		{
+			crouching = false;
 			speed = sprintSpeed;
-		} else {
+		}
+		else if (Input.GetKey(crouchKey))
+		{
+			crouching = true;
+			speed = crouchSpeed;
+		}
+		else
+		{
+			crouching = false;
 			speed = walkSpeed;
+		}
+
+		if (crouching)
+		{
+			Debug.Log("Crouching");
+			// Crouch down
+			charController.height = crouchHeight;
+			charController.center = new Vector3(originalCenter.x, originalCenter.y * crouchHeight / originalHeight, originalCenter.z);
+		}
+		else
+		{
+			Debug.Log("Standing");
+			// Stand up
+			charController.height = originalHeight;
+			charController.center = originalCenter;
 		}
 
 		//transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
